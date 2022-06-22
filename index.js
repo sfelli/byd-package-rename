5833b72745abf03769b81ff0548ec0906302482f
sfelli <safwen.felli-ext@sixense-group.com>
2022-06-22 18:06:11 +0200

    feat: use projectsPath as generic param

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const glob = require("fast-glob");
 
 try {
-  const projectsPath = core.getInput('projects-path') || './projects';
+  const projectsPath = core.getInput('projects-path') || './projects/**/package.json';
   const oldPrefix = core.getInput('old-prefix') || 'sixense';
   const newPrefix = core.getInput('new-prefix') || 'sxd-platform';
   const publishRegistry= core.getInput('publish-registry') || 'https://npm.pkg.github.com/';
@@ -13,7 +13,7 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
 
-  glob.sync(`${projectsPath}/**/package.json`).forEach((file) => {
+  glob.sync(`${projectsPath}`).forEach((file) => {
     const package = require(path.resolve(file));
     if (package.name) {
       package.publishConfig = {
