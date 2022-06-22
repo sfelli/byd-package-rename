6173a22889d19a55e4c2ba2b0797cf0c19ebd0f2
sfelli <safwen.felli-ext@sixense-group.com>
2022-06-22 11:20:34 +0200

    feat: implment the rename

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const path = require('path');
 
 try {
-  const nameToGreet = core.getInput('projects-path');
+  const projectsPath = core.getInput('projects-path') || './projects';
+  const oldPrefix = core.getInput('old-prefix') || 'sixense';
+  const newPrefix = core.getInput('new-prefix') || 'sxd-platform';
+  const publishRegistry= core.getInput('publish-registry') || 'https://npm.pkg.github.com/';
+
   console.info(`Beyong packages rename running !`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.info(`The event payload: ${payload}`);
+
+  glob.sync(`${projectsPath}/**/package.json`).forEach((file) => {
+    const package = require(path.resolve(file));
+    if (package.name) {
+      package.publishConfig = {
+        registry: publishRegistry,
+      };
+      package.name = package.name.replace(oldPrefix, newPrefix);
+      require('fs').writeFileSync(file, JSON.stringify(package, null, 2));
+    }
+    console.info(`Beyond packages renamed !`,package.name);
+  });
+
+
 } catch (error) {
   core.setFailed(error.message);
 }
